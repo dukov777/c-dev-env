7c3a7d3334c5a29dd1a685b6734ca38eb6dca97f
Petar Lalov <petar.lalov@gmail.com>
2022-08-29 01:15:03 +0300

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# http://google.github.io/googletest/quickstart-cmake.html
+
 docker build -t c-dev-env-1 \
     --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_ed25519)" \
     --build-arg SSH_PUBLIC_KEY="$(cat ~/.ssh/id_ed25519.pub)" \
